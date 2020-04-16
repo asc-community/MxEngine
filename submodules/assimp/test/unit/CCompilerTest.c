d73d0ec833dad5f2dd0b5ecab200e807bdb30cef
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2020-04-16 15:45:26 +0300

    fixed mistake from prev commit

--- submodules/assimp/test/unit/CCompilerTest.c
+++
@@ -1,9 +0,0 @@
-
-/* This is just a small test to check whether Assimp's API compiles from C */
-
-#include "Vendors/assimp/postprocess.h"
-#include "Vendors/assimp/scene.h"
-#include "Vendors/assimp/version.h"
-#include "Vendors/assimp/config.h"
-#include "Vendors/assimp/cimport.h"
-#include "Vendors/assimp/cexport.h"
