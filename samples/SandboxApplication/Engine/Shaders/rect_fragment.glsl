d7fd7204aaf29cf77785601eb07fdbb0e4fd5f70
Sasha.potapov2002 <Sasha.potapov2002@yandex.ru>
2021-03-18 01:32:38 +0300

    removed shader folder

--- samples/SandboxApplication/Engine/Shaders/rect_fragment.glsl
+++
@@ -1,8 +0,0 @@
-out vec4 Color;
-in vec2 TexCoord;
-uniform sampler2D tex;
-
-void main()
-{
-	Color = texture(tex, TexCoord).rgba;
-}
--- samples/Sponza/Engine/Shaders/rect_vertex.glsl
+++
@@ -1,9 +0,0 @@
-layout(location = 0) in vec4 position;
-
-out vec2 TexCoord;
-
-void main()
-{
-	gl_Position = position;
-	TexCoord = position.xy * 0.5 + vec2(0.5);
-}
