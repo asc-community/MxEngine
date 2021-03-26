a05f4e14664bfaf5cf93dfe075392515d3aac916
Momo <sasha.potapov2002@yandex.ru>
2021-03-27 00:59:43 +0300

    improved cmake scripts

--- cmake/project_install.cmake
+++ cmake/project_install.cmake
@@ -1,21 +1,24 @@
 function(install_mxengine_project EXECUTABLE_NAME)
     set(FULL_EXECUTABLE_FILENAME ${CMAKE_CURRENT_BINARY_DIR}/${EXECUTABLE_NAME}${CMAKE_EXECUTABLE_SUFFIX})
-    # copy all shared libraries to the current binary directory in case user wants to ship the executable
+
+    # copy all shared libraries to the current binary and source directories in case user wants to ship the executable
+    foreach(LIB_NAME ${LIBRARY_NAME} ${MxEngine_SHARED_LIBRARIES})
     add_custom_command(TARGET ${EXECUTABLE_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E
-        copy ${MxEngine_SHARED_LIBRARIES} "${CMAKE_CURRENT_BINARY_DIR}"
+        copy "$<TARGET_FILE:${LIB_NAME}>" "$<TARGET_FILE_DIR:${EXECUTABLE_NAME}>"
         || (exit 0)
     )
-    # copy all shared libraries to the source folder in case user want to launch application with his resources from there
     add_custom_command(TARGET ${EXECUTABLE_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E
-        copy ${MxEngine_SHARED_LIBRARIES} "${CMAKE_CURRENT_SOURCE_DIR}"
+        copy "$<TARGET_FILE:${LIB_NAME}>" "${CMAKE_CURRENT_SOURCE_DIR}"
         || (exit 0)
     )
+    endforeach()
+
     # copy executable to the source folder to access user project resources
     add_custom_command(TARGET ${EXECUTABLE_NAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E
-        copy "${FULL_EXECUTABLE_FILENAME}" "${CMAKE_CURRENT_SOURCE_DIR}"
+        copy "$<TARGET_FILE_DIR:${EXECUTABLE_NAME}>" "${CMAKE_CURRENT_SOURCE_DIR}"
         || (exit 0)
     )
 
