5a317c6ec37a445ec20595746156ab69f17be068
fall2019 <53587145+fall2019@users.noreply.github.com>
2023-04-19 23:05:24 +0800

    1. fix tracing step  2. improve performance

--- src/Platform/OpenGL/Shaders/god_ray_fragment.glsl
+++ src/Platform/OpenGL/Shaders/god_ray_fragment.glsl
@@ -31,25 +31,30 @@ void main()
     vec3 currentColor = texture(cameraOutput, TexCoord).rgb; 
     
     const float maxSteps =  255.0f;
-	const float sampleStep = 1.5f;
+	float sampleStep = 1.5f;
 	float numOfStep = fragDistance/sampleStep;
-	numOfStep = clamp(numOfStep,0.f,maxSteps);
+	numOfStep = clamp(numOfStep,100.f,maxSteps);
+    sampleStep=fragDistance/numOfStep;//avoid if statement
 	
     float randomness = noise(TexCoord) * .6;//producing blur to decrease sampling rate
     for (int lightIndex = 0; lightIndex < lightCount; lightIndex++)
     {
-		float illum = 0.0f;
+        float illum = 0.0f;
 		for (float i = 0.0f; i < numOfStep; i++) 
 		{
-			vec3 pos = camera.position + sampleStep*fragDirection*(i+randomness );
-			float shadowFactor = calcShadowFactorCascade(
-				vec4(pos,1.0), 
-				lights[lightIndex],
-				lightDepthMaps[lightIndex]);
+			vec3 pos = camera.position + sampleStep*fragDirection*(i+randomness);
+
+            //cal shadow without cascaded sampling
+            const vec2 textureSplitSize = vec2(1.01, 0.99) / DirLightCascadeMapCount;
+            vec4 textureLimitsXY = vec4(vec2(0.f, 1.f) * textureSplitSize, 0.001, 0.999);
+            vec4 fragLightSpace = lights[lightIndex].transform[0] * vec4(pos,1.0);
+            vec3 projectedPositions  = fragLightSpace.xyz / fragLightSpace.w;
+            float shadowFactor  = calcShadowFactor2D(projectedPositions , lightDepthMaps[lightIndex], textureLimitsXY, 0.002);
+
 			illum += smoothstep(0.0f, 1.0f, shadowFactor);
 		}
 		illum /= numOfStep;
-		currentColor = mix(currentColor, lights[lightIndex].color.rgb/255.0f, pow(illum, 0.7f));
+		currentColor = mix(currentColor, lights[lightIndex].color.rgb/255.0f, illum);
     }
 
 	OutColor = vec4(currentColor,1.f);
