22f3a242997f8576bd98b938b48d7bea3cd12d5f
fall2019 <53587145+fall2019@users.noreply.github.com>
2023-04-29 12:24:42 +0800

    auto format

--- src/Platform/OpenGL/Shaders/god_ray_fragment.glsl
+++ src/Platform/OpenGL/Shaders/god_ray_fragment.glsl
@@ -10,9 +10,9 @@ uniform sampler2D depthTex;
 
 struct Camera
 {
-    vec3 position;
-    mat4 invViewProjMatrix;
-    mat4 viewProjMatrix;
+	vec3 position;
+	mat4 invViewProjMatrix;
+	mat4 viewProjMatrix;
 };
 
 uniform sampler2D cameraOutput;
@@ -30,72 +30,72 @@ uniform float maxDistance;
 //sample only once without extra interpolation
 float godRayShadowFactor2D(vec3 coords, sampler2D depthMap, vec4 textureLimitsXY, float bias)
 {
-    float compare = coords.z - bias;
-    
-    const float lod = getTextureLodLevel(coords.xy);
-    vec2 texelSize = textureSize(depthMap, 0);
-    vec2 texelSizeInv = 1.0 / texelSize;
-    vec2 pixelPos = coords.xy * texelSize + vec2(0.5);
-    vec2 fracPart = fract(pixelPos);
-    vec2 startTexel = (pixelPos - fracPart) * texelSizeInv;
- 	float res = sampleShadowMap(depthMap, startTexel + vec2( 0.0,  0.0) * texelSizeInv, lod, compare); 
+	float compare = coords.z - bias;
+
+	const float lod = getTextureLodLevel(coords.xy);
+	vec2 texelSize = textureSize(depthMap, 0);
+	vec2 texelSizeInv = 1.0 / texelSize;
+	vec2 pixelPos = coords.xy * texelSize + vec2(0.5);
+	vec2 fracPart = fract(pixelPos);
+	vec2 startTexel = (pixelPos - fracPart) * texelSizeInv;
+	float res = sampleShadowMap(depthMap, startTexel + vec2(0.0, 0.0) * texelSizeInv, lod, compare);
 	return pow(clamp(res, 0.0, 1.0), 8.0);
 }
 float godRayShadowSampler(vec3 position, DirLight light, sampler2D shadowMap)
 {
 	const float bias = 0.002;
 	const vec2 textureSplitSize = vec2(1.01, 0.99) / DirLightCascadeMapCount;
-	for(int cascadeIndex = 0; cascadeIndex<DirLightCascadeMapCount; cascadeIndex++)
+	for (int cascadeIndex = 0; cascadeIndex < DirLightCascadeMapCount; cascadeIndex++)
 	{
 		float fIndex = float(cascadeIndex);
-		vec4 textureLimitsXY = vec4(vec2(fIndex,fIndex + 1.f) * textureSplitSize, 0.001, 0.999);
-		vec4 fragLightSpace = light.transform[cascadeIndex] * vec4(position,1.0);
-		vec3 projectedPosition  = fragLightSpace.xyz / fragLightSpace.w;
-		if (projectedPosition.x > textureLimitsXY[1] || projectedPosition.x < textureLimitsXY[0]||
-			projectedPosition.y > textureLimitsXY[3] || projectedPosition.y < textureLimitsXY[2]||
+		vec4 textureLimitsXY = vec4(vec2(fIndex, fIndex + 1.f) * textureSplitSize, 0.001, 0.999);
+		vec4 fragLightSpace = light.transform[cascadeIndex] * vec4(position, 1.0);
+		vec3 projectedPosition = fragLightSpace.xyz / fragLightSpace.w;
+		if (projectedPosition.x > textureLimitsXY[1] || projectedPosition.x < textureLimitsXY[0] ||
+			projectedPosition.y > textureLimitsXY[3] || projectedPosition.y < textureLimitsXY[2] ||
 			projectedPosition.z > 1.0 - bias || projectedPosition.z < bias)
 		{
 			continue;
 		}
-		return godRayShadowFactor2D(projectedPosition , shadowMap, textureLimitsXY, bias);
+		return godRayShadowFactor2D(projectedPosition, shadowMap, textureLimitsXY, bias);
 	}
 	return 1.0f;
 }
 
 void main()
 {
-    FragmentInfo fragment = getFragmentInfo(TexCoord, albedoTex, normalTex, materialTex, depthTex, camera.invViewProjMatrix);
-    vec3 camera2Frag = fragment.position-camera.position;
-    float fragDistance = length(camera2Frag);
-    vec3 fragDirection = normalize(camera2Frag);
-    vec3 currentColor = texture(cameraOutput, TexCoord).rgb; 
-    
-	
-    float randomness = noise(TexCoord) * .6;//producing blur to decrease sampling rate
-    for (int lightIndex = 0; lightIndex < lightCount; lightIndex++)
-    {
-        float illum = 0.0f;
+	FragmentInfo fragment = getFragmentInfo(TexCoord, albedoTex, normalTex, materialTex, depthTex, camera.invViewProjMatrix);
+	vec3 camera2Frag = fragment.position - camera.position;
+	float fragDistance = length(camera2Frag);
+	vec3 fragDirection = normalize(camera2Frag);
+	vec3 currentColor = texture(cameraOutput, TexCoord).rgb;
+
+
+	float randomness = noise(TexCoord) * .6;//producing blur to decrease sampling rate
+	for (int lightIndex = 0; lightIndex < lightCount; lightIndex++)
+	{
+		float illum = 0.0f;
 		float i = 0.0f;
 		float stp = sampleStep;
 		vec3 pos = camera.position;
-		for (; i < maxSteps; i++) 
+		for (; i < maxSteps; i++)
 		{
-			pos = camera.position + stp*fragDirection*(i+randomness);
-			if(fragDistance <= distance(pos,camera.position))
+			pos = camera.position + stp * fragDirection * (i + randomness);
+			if (fragDistance <= distance(pos, camera.position))
 				break;
 			stp *= stepIncrement;
-			
+
 			float shadowFactor = godRayShadowSampler(pos, lights[lightIndex], lightDepthMaps[lightIndex]);
 
-			illum += clamp(shadowFactor,0.0f, 1.0f);
+			illum += clamp(shadowFactor, 0.0f, 1.0f);
 		}
 		illum /= i;
-		float distanceTraveled = length(camera.position-pos);
-		illum *= distanceTraveled/maxDistance;
-		illum = clamp(illum,0.0,0.7);
+		float distanceTraveled = length(camera.position - pos);
+		illum *= distanceTraveled / maxDistance;
+		illum = clamp(illum, 0.0, 0.7);
 		float dropletInTheAir = 80.0;//todo: this should be replaced to certain function that clac influence to light intensity
 		currentColor = mix(currentColor, lights[lightIndex].color.rgb / dropletInTheAir, illum);
-    }
+	}
 
-	OutColor = vec4(currentColor,1.f);
+	OutColor = vec4(currentColor, 1.f);
 }
